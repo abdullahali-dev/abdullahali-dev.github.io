989e176e773bc6c7be498529fb0b26e5262146d7
abdullahali-dev <abdullah.clg@hotmail.com>
2022-07-05 22:55:15 +0300

    v 0.3.3 - manifest - PWA app

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+window.onload = () => {
+  "use strict";
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js");
+  }
+};
+
 function Init() {
   $("#NewGameBtn").popover({
     content: "اضغط هنا للبدء",
